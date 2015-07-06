a5ac5460c982c390fbd4d4c45030d6c85927f59f
Rash26 <rash261292@gmail.com>
2015-07-06 14:23:00 +0600

    Added_interface
    Added_interface

---
+++ Calculator/Calculator/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void bthExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btClear_Click(object sender, EventArgs e)
+        {
+            tbFirstValue.Clear();
+            tbSecondValue.Clear();
+            tbResult.Clear();
+        }
+    }
+}
